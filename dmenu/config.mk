09ade3a63c858fc69c9d3dac6fc648166b91c2cf
Christian Garbs <mitch@cgarbs.de>
2011-12-12 22:32:58 +0100

    permanently import dmenu sources
    Import dmenu 4.4.1 because getting rid of the patchsets for dwm was
    great.  Also, the repository is now self-contained and needs no
    downloads from suckless.org
    
    I'll see how I'll include future upstream versions as I can't just
    track the original dmenu repository.

---
+++ dmenu/config.mk
@@ -0,0 +1,25 @@
+# dmenu version
+VERSION = 4.4.1
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# Xinerama, comment if you don't want it
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# includes and libs
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS}
+
+# flags
+CPPFLAGS = -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CFLAGS   = -ansi -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  = -s ${LIBS}
+
+# compiler and linker
+CC = cc
