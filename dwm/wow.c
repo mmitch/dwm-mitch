562bc049825b01002e4682c25713c488a296921f
Christian Garbs <mitch@cgarbs.de>
2012-02-23 23:40:25 +0100

    WoW layout: shrink empty space by 50%
    otherwise I'll always have to do it manually after switching to that layout
    
    also refactor it a bit

--- dwm/wow.c
+++ dwm/wow.c
@@ -1,15 +1,17 @@
 void
 wow(unsigned int s) {
 	Client *c;
+	int wempty;
 	
 	domwfact[s] = True;
-	dozoom[s] = False;        
+	dozoom[s] = False;
+	wempty = (0.5 - (mwfact[s][selws[s]-1] / 2)) * waw[s];
         for(c = clients; c; c = c->next)
 		if (c->screen == s)
 			if (isvisible(c)) {
 				unban(c);
 				if(! c->isfloating)
-					resize(c, wax[s] + ( 1 - mwfact[s][selws[s]-1] ) * waw[s], way[s], waw[s] - 2 * BORDERPX - ( 1 - mwfact[s][selws[s]-1] ) * waw[s], wah[s] - 2 * BORDERPX, True);
+					resize(c, wax[s] + wempty, way[s], waw[s] - 2 * BORDERPX - wempty, wah[s] - 2 * BORDERPX, True);
 			} else {
 				ban(c);
 			}
