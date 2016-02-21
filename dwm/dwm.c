c8eb97c14d0b88861680fcbbce6fd46aca8d15d0
Christian Garbs <mitch@cgarbs.de>
2016-02-21 19:04:21 +0100

    remove unneeded code
    now that I know what the codes does, I know I don't need it :)

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -485,13 +485,6 @@ clientmessage(XEvent *e)
 		if (cme->data.l[1] == netatom[NetWMFullscreen] || cme->data.l[2] == netatom[NetWMFullscreen])
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
 				      || (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->ismax)));
-/*	} else if (cme->message_type == netatom[NetActiveWindow]) {
-		if (!ISVISIBLE(c)) {
-			c->mon->seltags ^= 1;
-			c->mon->tagset[c->mon->seltags] = c->tags;
-		}
-		pop(c);
-		} */
 }
 
 void
