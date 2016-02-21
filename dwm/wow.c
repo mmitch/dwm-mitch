8b4bea3715a8bf6f575146beae1d09cef9c9b2d8
Christian Garbs <mitch@cgarbs.de>
2016-02-21 19:16:01 +0100

    convert isvisible() to a macro
    wow.c must be converted, too (only in this branch)

--- dwm/wow.c
+++ dwm/wow.c
@@ -8,7 +8,7 @@ wow(unsigned int s) {
 	wempty = (0.5 - (mwfact[s][selws[s]-1] / 2)) * waw[s];
         for(c = clients; c; c = c->next)
 		if (c->screen == s) {
-			if (isvisible(c)) {
+			if (ISVISIBLE(c)) {
 				unban(c);
 				if(! c->isfloating)
 					resize(c, wax[s] + wempty * (sx[s] != 0), way[s], waw[s] - 2 * BORDERPX - wempty, wah[s] - 2 * BORDERPX, True);
